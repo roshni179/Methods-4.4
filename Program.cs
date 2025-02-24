0b1c4f3a368600c051bc37c07d29eebbe91e4775
701030552 <701030552@2260I-L29-031>
2025-02-24 14:45:00 -0500

    cont

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Methods_4._4
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Methods_4._4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ascii_art();
+            Console.WriteLine();
+            Console.WriteLine();
+            ascii_art();
         }
+
+        public static void ascii_art()
+
+        {
+            Console.WriteLine("/\\                   /\\");
+            Console.WriteLine("/ \\'._   (\\_/)   _.'/ \\");
+            Console.WriteLine("/_.''._'--('.')--'_.''._\\");
+            Console.WriteLine("| \\_ / `;=/ \" \\=;` \\ _/ |");
+
+
+        }
+
     }
 }
