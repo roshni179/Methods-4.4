05011c1f37596fa5f82a2af53b4f4c8df71ce1ad
701030552 <701030552@2260I-L29-031>
2025-02-25 14:09:30 -0500

    done

--- Program.cs
+++ Program.cs
@@ -6,22 +6,72 @@ namespace Methods_4._4
     {
         static void Main(string[] args)
         {
+            
             ascii_art();
             Console.WriteLine();
             Console.WriteLine();
-            ascii_art();
+            ascii2();
+            Console.WriteLine();
+            Console.WriteLine();
+            ascii_dolphin();
+            Console.WriteLine();
+            Console.WriteLine();
+            knock();
         }
 
         public static void ascii_art()
 
         {
-            Console.WriteLine("/\\                   /\\");
-            Console.WriteLine("/ \\'._   (\\_/)   _.'/ \\");
-            Console.WriteLine("/_.''._'--('.')--'_.''._\\");
-            Console.WriteLine("| \\_ / `;=/ \" \\=;` \\ _/ |");
+            Console.WriteLine("  /\\                 /\\");
+            Console.WriteLine(" / \\'._    (\\_/)   _.'/ \\");
+            Console.WriteLine(" /_.''._'--('.')--'_.''._\\");
+            Console.WriteLine(" | \\_ / `;=/ \" \\=;` \\ _/ |");
+            Console.WriteLine(" \\/ `\\__|` \\___/`|__/`  \\/");
+            Console.WriteLine("          \\(/|\\)/");
+            Console.WriteLine("           \" ` \"               ");
 
+          
+        }
+         public static void ascii2()
+        {
+            Console.WriteLine("  /\\_/\\");
+            Console.WriteLine(" ( o.o )");
+            Console.WriteLine("  > ^ <");
+            
+        }
+        
+        public static void ascii_dolphin()
+        {
+            Console.WriteLine("        ,");
+            Console.WriteLine("     __)\\_");
+            Console.WriteLine("(\\_.-'    a`-.");
+            Console.WriteLine("(/~~````(/~^^`");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void knock()
+        {
+            Console.WriteLine("Knock Knock....");
+            Thread.Sleep(500);
+            Console.ReadLine();
+            Console.WriteLine("Who's there?");
+            Thread.Sleep(500);
+            Console.ReadLine();
+            Console.WriteLine("water");
+            Thread.Sleep(500);
+            Console.ReadLine();
+            Console.WriteLine("water who");
+            Thread.Sleep(500);
+           
+            Console.ReadLine();
+            Console.WriteLine("WATER YOU DOING TODAY");
+           
 
         }
+        
+
+
 
     }
 }
